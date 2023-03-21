a07b888743f896defd490bd69d94953e58b9fb5d
Leonardo Secotaro <leonardosecotaro@192.168.1.6>
2023-03-21 12:41:49 -0300

    add more  to ignore

--- target/classes/db/migration/V1.0__create_schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE endpoints_request_historical(
-   id SERIAL PRIMARY KEY,
-   endpoint VARCHAR(40) NOT NULL,
-   params VARCHAR(60) NOT NULL,
-   response VARCHAR(60) NOT NULL,
-   date_time TIMESTAMP NOT NULL DEFAULT NOW()
-);
